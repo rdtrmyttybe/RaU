e063dc463a46618e571788abb8ad205d292652f1
browjob <32989588+browjob@users.noreply.github.com>
2022-01-06 21:42:27 +0200

    Update ffmpeg.js

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,7 +1,9 @@
 const { exec } = require('child_process');
 
-function startStream(ytlink) {
+const args = process.argv.slice(2)[0]
 
+function startStream(ytlink) {
+    console.log(ytlink)
     ls = exec(`bash str3_ytdlp.sh "${ytlink}"`);
 
     ls.stdout.on('data', (data) => {
@@ -15,25 +17,4 @@ function startStream(ytlink) {
     })
 }
 
-startStream('https://youtu.be/5pTKYtBaJSU')
-
-
-
-// function start_stream(m3u8_link) {
-
-//     ls = exec(`ffmpeg -re -i pipe: -http_persistent 0 -c copy -f flv rtmp://5.tcp.eu.ngrok.io:17771/live/r9pro`);
-
-
-//     ls.stdout.on('data', (data) => {
-//         console.log(data);
-//     });
-//     ls.stderr.on('data', (data) => {
-//         console.error(data);
-//     });
-//     ls.on('close', (code) => {
-//         console.log(code);
-//     })
-// }
-
-
-// yt-dlp --hls-use-mpegts --hls-prefer-ffmpeg --skip-unavailable-fragments --no-part -o - -f "bv*[height<=720]+ba/b[height<=720] / wv*+ba/w" --cookies cookies.txt $ytlink | ffmpeg -re -i pipe: -http_persistent 0 -c copy -f flv rtmp://live.restream.io/live/re_4173632_70f5420c2d7c94a5f44b
+startStream(args)
